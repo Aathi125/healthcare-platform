c1ba2064bc7b59757c3dbf8155b8f6d3b28d81ec
Fathima Aathika <atkilmdn1125@gmail.com>
2026-04-17 03:55:32 +0530

    feat(api-gateway): integrate all microservices routes

--- server/services/auth-service/src/utils/audit.js
+++
@@ -1,7 +0,0 @@
-const AuditLog = require("../models/AuditLog");
-
-async function writeAudit({ action, actorUserId = null, targetUserId = null, ip = null, meta = {} }) {
-  await AuditLog.create({ action, actorUserId, targetUserId, ip, meta });
-}
-
-module.exports = { writeAudit };
