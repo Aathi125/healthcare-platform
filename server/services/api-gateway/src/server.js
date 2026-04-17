d62ab52b9b7c01f5c45fff5b65dad7b415907eb9
Maneesha Hettiarachchi <maneesha.prabodani1996@gmail.com>
2026-04-17 10:12:56 +0530

    chore(api-gateway): remove old gateway

--- server/services/api-gateway/src/server.js
+++
@@ -1,8 +0,0 @@
-require("dotenv").config();
-const app = require("./app");
-
-const port = Number(process.env.PORT) || 8090;
-
-app.listen(port, () => {
-  console.log(`api-gateway listening on http://localhost:${port}`);
-});
